675846a5fc4df6efc62858ee886ef5d7eb73ff62
Jam Gabitanan <77550281+refuerzomaj@users.noreply.github.com>
2022-03-13 12:11:52 +0800

    Add files via upload

---
+++ Augmented Reality App for AMT/Assets/ComponentsScript2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class componentScript2 : MonoBehaviour
+{
+    public GameObject component;
+    public GameObject onBtn;
+    public GameObject offBtn;
+    public GameObject text;
+    public void hideComponents()
+    {
+        component.gameObject.SetActive(false);
+        onBtn.gameObject.SetActive(true);
+        offBtn.gameObject.SetActive(false);
+        text.gameObject.SetActive(true);
+    }
+    public void showComponents()
+    {
+        component.gameObject.SetActive(true);
+        onBtn.gameObject.SetActive(false);
+        offBtn.gameObject.SetActive(true);
+    }
+    public void hideAll()
+    {
+        component.gameObject.SetActive(false);
+        onBtn.gameObject.SetActive(false);
+        offBtn.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+    }
+}
